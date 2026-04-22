0d22b763778995361b287ad6614b9432e2323936
LucasTCamp <lucastcampbell@outlook.com>
2026-04-22 08:20:28 -0400

    added empty list test

--- test_gradebook.py
+++ test_gradebook.py
@@ -27,4 +27,8 @@ def test_is_passing_invalid_type():
         is_passing("hello")
 
 def test_average_works():
-    assert average([80, 90, 70]) == 80.0  
+    assert average([80, 90, 70]) == 80.0
+
+def test_average_empty_list():
+    with pytest.raises(ValueError):
+        average([])
